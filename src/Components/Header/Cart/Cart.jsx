2ffe80c96fbd02b688f41766546af0c7d214b3d3
Alamgirislam420 <khanmdalamgir242@gmail.com>
2023-11-25 10:12:05 +0600

    show data added

--- src/Components/Header/Cart/Cart.jsx
+++ src/Components/Header/Cart/Cart.jsx
@@ -9,18 +9,25 @@ const Cart = (props) => {
     
     let totalPrice = 0;
     for (const product of cart)
-        totalPrice = totalPrice + product.price;
+        totalPrice = totalPrice + product.price * product.quantity;
     
     let totalShipping = 0;
     for (const product of cart)
         totalShipping = totalShipping + product.shipping;
+    let quantity = 0;
+    for(const product of cart)
+        quantity = quantity + product.quantity;
+    for(const product of cart)
+    if (product.quantity === 0) {
+        product.quantity = 1;
+    }
     const tax = totalPrice * 7 / 100;
 
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${ totalShipping}</p>
             <p>Tax: ${ tax}</p>
