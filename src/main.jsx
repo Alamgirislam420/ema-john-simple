f0f450c07e14cf462faf1a6ed852b351fb45e948
Alamgirislam420 <khanmdalamgir242@gmail.com>
2023-12-06 21:06:47 +0600

    layout add to cart

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './Components/Header/Shop/Shop.jsx';
+import Home from './Components/Header/Layout/Home.jsx';
+import Orders from './Components/Header/Orders/Orders.jsx';
+import Inventory from './Components/Inventory/Inventory.jsx';
+import Login from './Components/Login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'Login',
+        element: <Login></Login>
+      }
+
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.
+  StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
