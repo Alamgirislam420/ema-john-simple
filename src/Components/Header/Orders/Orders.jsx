35fe38235606bbd6d0603c2a7ec2dfd6f5179729
Alamgirislam420 <khanmdalamgir242@gmail.com>
2023-12-11 22:03:20 +0600

    add to eventhandler

--- src/Components/Header/Orders/Orders.jsx
+++ src/Components/Header/Orders/Orders.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../../ReviewItem/ReviewItem';
 import Product from '../Product/Product';
-import { key } from 'localforage';
+import { key, removeItem } from 'localforage';
 import './Orders.css';
+import { faRemove } from '@fortawesome/free-solid-svg-icons';
 
 const Orders = () => {
-    const cart = useLoaderData();
-    console.log(cart);
+    const saveCart = useLoaderData();
+    const [cart, setCart] = useState(saveCart);
+    
+    const handleRemoveFromCart = (id) => {
+        const remainig = cart.filter(product => product.id !== id);
+        setCart(remainig);
+        // removeFormDb(id);
+
+    }
+
     return (
         <div className='shop-container'>
             <div className='review-container'>
@@ -16,6 +25,7 @@ const Orders = () => {
                     cart.map(product => <ReviewItem
                         key={product.id}
                         product={product}
+                        handleRemoveFromCart={handleRemoveFromCart}
                     ></ReviewItem>)
                 }
             </div>
